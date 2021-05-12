286d5cf4cfb4fe71c6cbfee0411e28345dccd11b
MoD3223 <44062662+MoD3223@users.noreply.github.com>
2021-05-12 14:01:03 +0200

    Add project files.

---
+++ Keep Hydrated/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Keep_Hydrated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Hydration();
+            }
+
+
+        }
+
+        public static void Hydration()
+        {
+            Console.WriteLine("Keep Hydrated!");
+            int litres;
+            double timeInHours;
+            string stringTimeInHours;
+            stringTimeInHours = Console.ReadLine();
+
+
+            if (stringTimeInHours.Contains("."))
+            {
+                stringTimeInHours.Replace(".",",");
+            }
+            if (Double.TryParse(stringTimeInHours, out timeInHours))
+            {
+                timeInHours /= 2;
+                litres = (int)timeInHours;
+
+                Console.WriteLine("Your water intake should be: " + litres);
+            }
+            else
+            {
+                Console.WriteLine("Error, couldn't calculate");
+                Console.WriteLine("Try Again!");
+            }
+        }
+
+
+
+    }
+}
